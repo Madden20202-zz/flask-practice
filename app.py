54007a800d22a61f6a1f5d02f4ed744ab8cf0869
austin <business.mocksock@gmail.com>
2022-06-08 17:25:23 -0500

    start of tutorial

--- app.py
+++ app.py
@@ -1,24 +1,15 @@
 from flask import Flask
 
-# Let's make a hello world app
+# Let's make a more complex app 
+# Increment counting app 
 
-# Creates an instance of the class
 app = Flask(__name__)
 
-# route allows the app to know what 
-# URL triggers the functions that 
-# are defined
-
-# methods specifies what HTTP methods are allowed
-# the default value is ['GET']
 @app.route('/', methods=['GET', 'POST'])
 
 def welcome():
     return "Hello World!"
 
-# This is a special variable in Python where
-# the value of a script name is taken and 
-# ensures that the app is only ran on the main file
 if __name__ == '__main__':
     # Runs the Flask app
 
