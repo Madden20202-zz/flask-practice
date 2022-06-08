6ad417b3f3f467cd7c5bdfea7eba6b162a2007aa
austin <business.mocksock@gmail.com>
2022-06-08 17:08:59 -0500

    fix route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 # Let's make a hello world app
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 
 def welcome():
     return "Hello World!"
