caa0a4c6073e19fd9b6fcedb8b42119369f43508
austin <business.mocksock@gmail.com>
2022-06-08 17:34:44 -0500

    add notes

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 
 # Let's make a more complex app 
-# Increment counting app 
+# Increment counting app with a name app within it
 
 app = Flask(__name__)
 
+# the route now becomes http://localhost:5000/<number here>
+# now multiple functions can be defined 
+# and called when the url is input
 @app.route('/<int:number>')
 
 def incrimenter(number):
+    # this will take the number in the URL and add 1
     return "We are at " + str(number+1)
 
 if __name__ == '__main__':
