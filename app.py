d6e232cf3ccfd5960076f306d4789cab3e914f67
austin <business.mocksock@gmail.com>
2022-06-09 06:03:14 -0500

    remove old code

--- app.py
+++ app.py
@@ -2,14 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# devs can return a status code when code executes
-# https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 
-@app.route('/')
-def teapot():
-    # this error code returns 'I am a Tea pot'
-    # a cute little joke
-    return "What is your favorite tea?", 418
 
 if __name__ == '__main__':
     # Runs the Flask app
