623e9cf873882794d50e6e6d003564dc4f4dcf9a
austin <business.mocksock@gmail.com>
2022-06-09 08:34:56 -0500

    clear out last lesson

--- app.py
+++ app.py
@@ -2,25 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# everything that has been done so far has 
-# been a request, something that has been 
-# read from the code in order to fulfill 
-# some task, which lately has just been
-# to display some data
 
-# lets have it perform something before the request is filfilled
-
-@app.before_request
-# Why use this?
-# This is exceptionally important to logging for the server
-# The code in this actually runs on the SERVER and this 
-# helps check for issues and fault points
-def before():
-    print("This could be code executed before each request!")
-
-@app.route('/home/')
-def greeting():
-    return "Hello, Welcome to the tutorial!"
 
 if __name__ == '__main__':
     # Runs the Flask app
