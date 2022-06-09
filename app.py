f192a412a4e210df9f2cfacba0d8f53ded0a0880
austin <business.mocksock@gmail.com>
2022-06-09 09:08:22 -0500

    simplify the app.run section

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 app.register_blueprint(home_bp, url_prefix='/home')
 app.register_blueprint(contacts_bp, url_prefix='/contact')
 
-if __name__ == '__main__':
-    # Runs the Flask app
-
-    # host specifies where the app runs
-    # default values are 127.0.0.1 and localhost
-    app.run(host='localhost', debug=True)
+# simplify the last mess of work, 
+# no if required 
+app.run(host='localhost', debug=True)
