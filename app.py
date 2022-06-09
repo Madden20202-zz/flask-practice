52f7cfefbafc1e7e6dcfdf8b60066157507e0ca1
austin <business.mocksock@gmail.com>
2022-06-09 05:51:54 -0500

    remove old code

--- app.py
+++ app.py
@@ -2,24 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Now apps will always redirect when something
-# is done, a login page is the best example of this
 
-# When something has a / at the end, 
-# the app will read it as a sort of directory,
-# where it can be added to
-# ex /home/posts
-@app.route('/home/')
-
-def home_page():
-    return "This is the Home Page"
-
-# This will not be able to move forward,
-# and will be a single page app
-@app.route('/contact')
-
-def contact_page():
-    return "Contacts would come up here"
 
 if __name__ == '__main__':
     # Runs the Flask app
