6fdefb97891828cab357216f43f78f94f0598ce7
austin <business.mocksock@gmail.com>
2022-06-09 09:50:41 -0500

    more syntax fixes

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from contacts import contacts_bp, home_bp
 
 app = Flask(__name__)
 
+# the url is localhost:5000/home but /home/hello works as well
 app.register_blueprint(home_bp, url_prefix='/home')
 app.register_blueprint(contacts_bp, url_prefix='/contact')
 
-# simplify the last mess of work, 
 # no if required 
 app.run(host='localhost', debug=True)
